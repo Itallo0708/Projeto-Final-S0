b4ced7862f50b82df42a691bbeeb5412b7cfc46b
itallo0708 <itallofcn@gmail.com>
2025-08-31 18:49:50 -0300

    feat: adiciona os mineradores

--- coleta_recursos.c
+++ coleta_recursos.c
@@ -39,11 +39,36 @@ void *coletar_madeira(void* ponteiro_id){
     pthread_exit(NULL);
 }
 
+// função para coletar pedra
+
+void *coleta_pedra( void* ponteiro_id ){
+    // recebe a id do minero
+    int id = *((int*)ponteiro_id);
+    free(ponteiro_id);
+
+    for (int i = 0; i < COLETA_TRABALHADOR; i++){
+        // tenta entrar na região crítica
+        sem_wait(&mutex_pedra);
+        // região crítica
+        pedra++;
+        printf("Minero %i coletou pedra! Total: %i\n", id, pedra);
+        // saiu da região crítica
+
+        // libera semáforo
+        sem_post(&mutex_pedra);
+        // simulação de tempo
+        usleep(100000 + rand() % 100000);
+    }
+    pthread_exit(NULL);
+}
+
 int main() {
     pthread_t lenhadores[N_LENHADORES];
+    pthread_t mineradores[M_MINERADORES];
 
     //incializar os semáforos 
     sem_init(&mutex_madeira, 0, 1);
+    sem_init(&mutex_pedra, 0, 1);
 
     printf("Inicizalizando a coleta de recursos\n");
 
@@ -52,7 +77,18 @@ int main() {
         int* id = malloc(sizeof(int));
         *id = i + 1;
         if (pthread_create(&lenhadores[i], NULL, coletar_madeira, (void*)id) != 0){
-            printf("Erro ao criar a thread do lenhador");
+            printf("Erro ao criar a thread dos lenhadores!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    // criar threads para os mineradores
+    for (int i = 0; i <M_MINERADORES; i++){
+        int* id = malloc(sizeof(int));
+        *id = i + 1;
+        
+        if (pthread_create(&mineradores[i], NULL, coleta_pedra, (void*)id) != 0){
+            printf("Erro ao criar threads dos mineradores!\n");
             exit(EXIT_FAILURE);
         }
     }
@@ -62,13 +98,21 @@ int main() {
         pthread_join(lenhadores[i], NULL);
     }
 
+    // aguarda a finalização das threads dos mineradores
+    for (int i = 0; i < M_MINERADORES; i++){
+        pthread_join(mineradores[i], NULL);
+    }
+
     // destroindo semaáforos após o uso
     sem_destroy(&mutex_madeira);
+    sem_destroy(&mutex_pedra);
 
     // resultados
     printf("\n Coleta Finalizada!\n");
     printf("Total de madeira: %i\n", madeira);
     printf("Total esperado de madeira na coleta: %i\n", N_LENHADORES * COLETA_TRABALHADOR);
+    printf("Total de pedra: %i\n", pedra);
+    printf("Total de pedra esperado: %i\n", M_MINERADORES * COLETA_TRABALHADOR);
 
     return 0;
 }
